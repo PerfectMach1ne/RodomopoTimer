2c68c6eaa7d253289eb63b32e2671c9a5fa3352b
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2022-12-10 15:31:15 +0100

    First GUI implementation attempt (faulty)

--- src/main/java/rodomopotimer/Main.java
+++ src/main/java/rodomopotimer/Main.java
@@ -2,16 +2,44 @@ package main.java.rodomopotimer;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.Container;
 
 public class Main {
+    private static JPanel cards;
+    private static JButton startClock;
+    private static JButton pauseClock;
+    private static JButton stopClock;
+
     private static void createAndShowGUI() {
         // Create and set up the window
         JFrame frame = new JFrame("Rodomopo Timer");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // A placeholder label from Java Swing tutorial (i forgor how to do it all)
-        JLabel label = new JLabel("Hello World");
-        frame.getContentPane().add(label);
+        // Create the timer label card for the card container
+        JPanel cardTimer = new JPanel();
+        JLabel timerLabel = new JLabel();
+        timerLabel.setText("<html><body>" + "<div style=\"font-size:72;\">" + "00:00" + "</div></body></html>");
+        cardTimer.add(timerLabel);
+
+        // Create the buttons card for the card container
+        JPanel cardButtons = new JPanel();
+        startClock = new JButton("Start");
+        pauseClock = new JButton("Pause");
+        stopClock = new JButton("Stop");
+        cardButtons.add(startClock);
+        cardButtons.add(pauseClock);
+        cardButtons.add(stopClock);
+
+        // Create the card container
+        cards = new JPanel(new CardLayout());
+        cards.add(cardTimer);
+        cards.add(cardButtons);
+        frame.add(cards);
 
         // Center the window
         frame.setLocationRelativeTo(null);
@@ -21,7 +49,13 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        com.formdev.flatlaf.FlatDarculaLaf.setup();
+        // Setup FlatLaf Darcula theme
+        try {
+            UIManager.setLookAndFeel( new com.formdev.flatlaf.FlatDarculaLaf() );
+        } catch( Exception ex ) {
+            System.err.println("Failed to initialize LaF");
+        }
+//        com.formdev.flatlaf.FlatDarculaLaf.setup();
 
         javax.swing.SwingUtilities.invokeLater(Main::createAndShowGUI);
     }
